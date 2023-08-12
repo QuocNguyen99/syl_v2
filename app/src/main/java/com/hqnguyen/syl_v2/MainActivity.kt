4c5ef2ae3f08c9eac7a00e37670a79df50b81e4c
Nguyên <huynhquocnguyen99@gmail.com>
2023-08-13 02:01:14 +0700

    change name theme

--- app/src/main/java/com/hqnguyen/syl_v2/MainActivity.kt
+++ app/src/main/java/com/hqnguyen/syl_v2/MainActivity.kt
@@ -10,13 +10,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.hqnguyen.syl_v2.ui.theme.Syl_v2Theme
+import com.hqnguyen.syl_v2.ui.theme.SYLTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Syl_v2Theme {
+            SYLTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -40,7 +40,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    Syl_v2Theme {
+    SYLTheme {
         Greeting("Android")
     }
 }
